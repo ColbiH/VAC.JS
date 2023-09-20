5f1577316cdd00b0acfd4a846f1ac2903b5bc360
ColbiH <colbihopkins@gmail.com>
2023-09-20 18:44:35 -0400

    BackEnd and FrontEnd sandbox pages created

---
+++ src/components/BackEnd.js
@@ -0,0 +1,14 @@
+import React from 'react';
+
+function BackEnd() {
+    return (
+        <div>
+            <h1>This is the playground for the Back End</h1>
+            {
+
+            }
+        </div>
+    );
+}
+
+export default BackEnd;
---
+++ src/components/FrontEnd.js
@@ -0,0 +1,14 @@
+import React from 'react';
+
+function FrontEnd() {
+    return (
+        <div>
+            <h1>This is the playground for the Front End</h1>
+            {
+
+            }
+        </div>
+    );
+}
+
+export default FrontEnd;
