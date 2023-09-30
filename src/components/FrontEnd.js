acccd0e933fa1dc710794cffd7aa3aa68b2caf3b
jevans33 <jevans3@ufl.edu>
2023-09-30 09:55:35 -0400

    Created test print pdf page; need to check functionality with backend

--- src/components/FrontEnd.js
+++ src/components/FrontEnd.js
@@ -1,13 +1,55 @@
 import React from 'react';
+import { Button, Flex, Img, InstUISettingsProvider, instructure, View} from "@instructure/ui";
+import Image from "../quiz.png";
+//import styled from "styled-components";
+
+
+// const Button = styled.button`
+//   background-color: black;
+//   color: white;
+//   font-size: 20px;
+//   padding: 10px 60px;
+//   border-radius: 5px;
+//   margin: 10px 0px;
+//   cursor: pointer;
+// `;
+
+function sayHello() {
+    alert('You clicked me!');
+}
 
 function FrontEnd() {
     return (
-        <div>
-            <h1>This is the playground for the Front End</h1>
-            {
+        <InstUISettingsProvider theme={instructure}>
+        <>
+            <Flex justifyItems = "space-around">
+                <Flex.Item shouldShrink shouldGrow textAlign = "center">
+                    <h1>Welcome to Vac.js!</h1>
+                </Flex.Item>
+            </Flex>
+            <Flex wrap = "wrap" justifyItems = "space-around" margin = "0 0 medium medium">
+                <Flex.Item
+                    size = "100px"
+                    shouldGrow
+                    padding = "medium"
+                    borderWidth = "medium"
+                    borderColor = "info">
+                    <Img
+                        constrain = "cover"
+                        alt="A placeholder image"
+                        src={Image} />
+                </Flex.Item>
 
-            }
-        </div>
+                <Flex.Item
+                    padding = "medium">
+                    <Button onClick={sayHello}>Print as a pdf</Button>
+                </Flex.Item>
+
+            {/*<View textAlign="center" as="div">*/}
+            {/*</View>*/}
+            </Flex>
+        </>
+        </InstUISettingsProvider>
     );
 }
 
