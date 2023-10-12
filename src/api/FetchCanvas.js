add4e13834e9476d24bc7eeeac5edc07e8343c47
ColbiH <colbihopkins@gmail.com>
2023-10-12 12:07:52 -0400

    Backend Demo Setup with Bearer Authorization removed

---
+++ src/api/FetchCanvas.js
@@ -0,0 +1,12 @@
+export async function FetchCanvas(url, options) {
+    try {
+        const response = await fetch(url, options);
+        if (!response.ok) {
+            throw new Error(`HTTP error! Status: ${response.status}`);
+        }
+        const data = await response.json();
+        return data;
+    } catch (error) {
+        throw error;
+    }
+}
---
+++ src/api/FetchQuizQuestions.js
@@ -0,0 +1,34 @@
+import React, { useEffect, useState } from 'react';
+import { FetchCanvas } from './FetchCanvas';
+import LaTeXBuilder from "../components/LaTeXBuilder";
+
+function FetchQuizQuestions() {
+    const [data, setData] = useState([]);
+    const url = 'https://cors-anywhere.herokuapp.com/https://ufl.instructure.com/api/v1/courses/471518/quizzes/1287636/questions';
+
+
+    useEffect(() => {
+        const options = {
+            method: 'GET',
+            headers: {
+                Authorization: 'Bearer '
+            }
+        };
+
+        FetchCanvas(url, options)
+            .then((result) => {
+                setData(result);
+            })
+            .catch((error) => {
+                console.error('Error fetching data:', error);
+            });
+    }, []);
+
+    return (
+        <div>
+            <LaTeXBuilder data={data}/>
+        </div>
+    );
+}
+
+export default FetchQuizQuestions;
