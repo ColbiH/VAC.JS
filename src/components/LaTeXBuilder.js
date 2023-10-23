045b4117611cdb01cc4f53e5743ab618ee073b44
lasallea <alyssal1021@gmail.com>
2023-10-23 18:32:43 -0400

    updated LaTeX Builder

--- src/components/LaTeXBuilder.js
+++ src/components/LaTeXBuilder.js
@@ -13,15 +13,9 @@ function extractContentBetweenPTags(inputString) {
     }
 }
 
-function LaTeXBuilder({ data }) {
-    if (!data || data.length === 0) {
-        return <div>Loading...</div>;
-    }
-
-    var q0 = extractContentBetweenPTags(data[0].question_text);
-    var q1 = extractContentBetweenPTags(data[1].question_text);
-
-    const LaTeXTemplate = "\\documentclass[addpoints]{exam}\n" +
+function Template(data) {
+    // Extract the desired values
+    let LaTeXTemplate = "\\documentclass[addpoints]{exam}\n" +
         "\\usepackage{comment}\n" +
         "\\usepackage{multicol}\n" +
         "\n" +
@@ -45,39 +39,83 @@ function LaTeXBuilder({ data }) {
         "% -------------------------------------------------------------\n" +
         "\n" +
         "% Here, the questions begin\n" +
-        "\\begin{questions}\n" +
-        "\n" +
-        "% MULTIPLE CHOICE QUESTION\n" +
-        "% ------------------------------------------------------------- \n" +
-        " \n" +
-        "\\question[5] " + q1 + "\n" +
-        " \n" +
-        "\\begin{checkboxes} \n" +
-        "\\choice " + data[1].answers[0].text + " \n" +
-        "\\choice " + data[1].answers[1].text + " \n" +
-        "\\choice " + data[1].answers[2].text + " \n" +
-        "\\choice " + data[1].answers[3].text + " \n" +
-        "\\end{checkboxes}\\vspace{1cm}\n" +
-        "% -------------------------------------------------------------\n" +
-        "\n" +
-        "% T/F QUESTION\n" +
-        "\\question[5] " + q0 + "\n" +
-        "\n" +
-        "\\begin{checkboxes}\n" +
-        "\\choice " + data[0].answers[0].text + " \n" +
-        "\\choice " + data[0].answers[1].text + " \n" +
-        "\\end{checkboxes}\\vspace{1cm}\n" +
-        "\n" +
-        "\\end{questions}\n" +
+        "\\begin{questions}\n"
+
+    if (Array.isArray(data) && data.length > 0) {
+        for (let i = 0; i < data.length; i++) {
+            let questionType = data[i].question_type;
+            let questionText = extractContentBetweenPTags(data[i].question_text);
+            let answerOptions = data[i].answers.map(answer => answer.text);
+            if (questionType === "multiple_choice_question") {
+                let multiChoiceQuestion = "\\question " + questionText + " \n" +
+                    "\\begin{checkboxes} \n"
+                LaTeXTemplate += multiChoiceQuestion;
+                for (let j = 0; j < answerOptions.length; j++) {
+                    LaTeXTemplate += "\\choice " + answerOptions[j] + " \n"
+                }
+                LaTeXTemplate += "\\end{checkboxes}\\vspace{1cm}\n"
+            }
+            if (questionType === "true_false_question") {
+                let trueFalseQuestion = "\\question " + questionText + " \n" +
+                    "\\begin{checkboxes} \n"
+                LaTeXTemplate += trueFalseQuestion;
+                for (let j = 0; j < answerOptions.length; j++) {
+                    LaTeXTemplate += "\\choice " + answerOptions[j] + " \n"
+                }
+                LaTeXTemplate += "\\end{checkboxes}\\vspace{1cm}\n"
+            }
+            if (questionType === "short_answer_question") {
+                let shortAnswerQuestion = "\\question " + questionText + "\\vspace{1cm}\n"
+                LaTeXTemplate += shortAnswerQuestion;
+            }
+            if(questionType === "fill_in_multiple_blanks_question"){
+                let multiBlankQuestion = "\\question " + questionText + "\\vspace{1cm}\n"
+                LaTeXTemplate += multiBlankQuestion;
+            }
+            if (questionType === "multiple_answers_question") {
+                let multiAnswersQuestion = "\\question " + questionText + " \n" +
+                    "\\begin{checkboxes} \n"
+                LaTeXTemplate += multiAnswersQuestion;
+                for (let j = 0; j < answerOptions.length; j++) {
+                    LaTeXTemplate += "\\choice " + answerOptions[j] + " \n"
+                }
+                LaTeXTemplate += "\\end{checkboxes}\\vspace{1cm}\n"
+            }
+            if(questionType === "multiple_dropdowns_question"){
+                let multiDropDownsQuestion = "\\question " + questionText + "\\vspace{1cm}\n"
+                LaTeXTemplate += multiDropDownsQuestion;
+            }
+            if(questionType === "numerical_question"){
+                let numQuestion = "\\question " + questionText + "\\vspace{1cm}\n"
+                LaTeXTemplate += numQuestion;
+            }
+            if(questionType === "essay_question"){
+                let essayQuestion = "\\question " + questionText + "\\vspace{5cm}\n"
+                LaTeXTemplate += essayQuestion;
+            }
+            if(questionType === "text_only_question"){
+                let textOnlyQuestion = "\\question " + questionText + "\n"
+                LaTeXTemplate += textOnlyQuestion;
+            }
+        }
+    }
+
+    LaTeXTemplate += "\\end{questions}\n" +
         "\\clearpage\n" +
         "\n" +
-        "\\end{document}"
-    ;
+        "\\end{document}";
+
+    return LaTeXTemplate;
+}
+
+function LaTeXBuilder({ data }) {
+    if (!data || data.length === 0) {
+        return <div>Loading...</div>;
+    }
 
     return (
         <div>
-            <LaTeXWasm template={LaTeXTemplate} />
-
+            <LaTeXWasm template={Template(data)} />
         </div>
     );
 }
