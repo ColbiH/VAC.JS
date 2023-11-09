337c80893c80132ab4ea3b58f4da021c3cd1c8de
sambarthelemy <sbarthelemy@ufl.edu>
2023-11-09 16:46:51 -0500

    reappplied stashed changes, components not loading

--- src/App.js
+++ src/App.js
@@ -7,29 +7,28 @@ import FrontEnd from "./legacy/FrontEnd";
 import Intro from "./components/Intro";
 import PrintVsGrade from "./components/PrintVsGrade";
 import './App.css';
-import FetchClassesAndQuizzes from "../api/FetchClassesAndQuizzes";
-import AssignmentTreeBrowser from "./AssignmentTreeBrowser";
-import AssignmentDisplay from "./AssignmentDisplay";
+import FetchClassesAndQuizzes from "./api/FetchClassesAndQuizzes";
+import AssignmentTreeBrowser from "./components/Grading/AssignmentTreeBrowser";
+import AssignmentDisplay from "./components/Grading/AssignmentDisplay";
 
 function App() {
     return (
         <div className="App">
             <Routes>
                 <Route path = "/" element = {<Intro />} />
+                {/*
                 <Route path = "/sam" element = {<AssignmentsDisplay/>} />
                 <Route path = "/assignmentsam" element = {<AssignmentsDisplay/>} />
+                */}
                 <Route path = "/jenny" element = {<Jenny />} />
                 <Route path = "/valentina" element = {<FrontEnd />} />
                 <Route path = "/backend" element = {<BackEnd />} />
                 <Route path = "/login" element = {<Login />}/>
                 <Route path = "/printvsgrade" element = {<PrintVsGrade />} />
                 <Route path = "/fetchclassesquizzes" element = {<FetchClassesAndQuizzes />} />
-<<<<<<< HEAD:src/components/App.js
                 <Route path = "/fetchclassassignments" element = {<AssignmentTreeBrowser />} />
                 <Route path = "/assignmenttable" element = {<AssignmentDisplay />} />
-=======
-                <Route path = "/fetchclassesassignments" element = {<FetchClassesAndAssignments />} />
->>>>>>> master:src/App.js
+                {/*<Route path = "/fetchclassesassignments" element = {<FetchClassesAndAssignments />} /> */}
             </Routes>
         </div>
     );
