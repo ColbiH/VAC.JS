c7aa415b236ae35593b44a89c1d859bf4f0adeb4
jevans33 <jevans3@ufl.edu>
2023-11-19 19:16:02 -0500

    Changed static table in AssignmentsDisplay to dynamic table in FetchAssignmentSubmissions; added buttons to FetchAssignmentSubmissions

--- src/components/Grading/AssignmentsDisplay.js
+++ src/components/Grading/AssignmentsDisplay.js
@@ -53,80 +53,80 @@ function AssignmentsDisplay() {
             {/*        />*/}
             {/*    </Modal>*/}
             {/*</div>*/}
-            <div>
-                <Sidebar/>
-            </div>
+            {/*<div>*/}
+            {/*    <Sidebar/>*/}
+            {/*</div>*/}
 
-            <div className="assignment-name">
-                <Text color="primary" size="x-large" weight="bold">Assignment name here</Text>
-            </div>
+            {/*<div className="assignment-name">*/}
+            {/*    <Text color="primary" size="x-large" weight="bold">Assignment name here</Text>*/}
+            {/*</div>*/}
 
-            <div className="download-button">
-                <Button color="secondary" margin="small">Download All  <IconDownloadLine/></Button>
-            </div>
+            {/*<div className="download-button">*/}
+            {/*    <Button color="secondary" margin="small">Download All  <IconDownloadLine/></Button>*/}
+            {/*</div>*/}
 
-            <div className="grade-button">
-                <Button color="primary" margin="small">Grade All  <IconGradebookExportLine/></Button>
-            </div>
+            {/*<div className="grade-button">*/}
+            {/*    <Button color="primary" margin="small">Grade All  <IconGradebookExportLine/></Button>*/}
+            {/*</div>*/}
 
-            <div className="toggle">
-                <Checkbox label="Publish grades" value="medium" variant="toggle" />
-            </div>
+            {/*<div className="toggle">*/}
+            {/*    <Checkbox label="Publish grades" value="medium" variant="toggle" />*/}
+            {/*</div>*/}
 
 
-            <div className="assignment-table">
-                <Table
-                    layout= 'auto'
-                    hover= 'true'
-                >
-                    <Table.Head>
-                        <Table.Row>
+            {/*<div className="assignment-table">*/}
+            {/*    <Table*/}
+            {/*        layout= 'auto'*/}
+            {/*        hover= 'true'*/}
+            {/*    >*/}
+            {/*        <Table.Head>*/}
+            {/*            <Table.Row>*/}
 
-                            <Table.ColHeader id="Name" width="20%">Student Name</Table.ColHeader>
-                            <Table.ColHeader id="ID" width="15%">SIS ID</Table.ColHeader>
-                            <Table.ColHeader id="Submission" width="15%">Submission</Table.ColHeader>
-                            <Table.ColHeader id="Download" width="20%">Download</Table.ColHeader> {/*shows if student's assignment has been downloaded or not*/}
-                            <Table.ColHeader id="Grade_Status" width="20%">Grade Status</Table.ColHeader>
-                            <Table.ColHeader id="Score" width="10%">Score</Table.ColHeader>
+            {/*                <Table.ColHeader id="Name" width="20%">Student Name</Table.ColHeader>*/}
+            {/*                <Table.ColHeader id="ID" width="15%">SIS ID</Table.ColHeader>*/}
+            {/*                <Table.ColHeader id="Submission" width="15%">Submission</Table.ColHeader>*/}
+            {/*                <Table.ColHeader id="Download" width="20%">Download</Table.ColHeader> /!*shows if student's assignment has been downloaded or not*!/*/}
+            {/*                <Table.ColHeader id="Grade_Status" width="20%">Grade Status</Table.ColHeader>*/}
+            {/*                <Table.ColHeader id="Score" width="10%">Score</Table.ColHeader>*/}
 
-                        </Table.Row>
-                    </Table.Head>
-                    <Table.Body>
-                        <Table.Row>
-                            <Table.RowHeader>Samantha Barthelemy</Table.RowHeader>
-                            <Table.Cell>40475908</Table.Cell>
-                            <Table.Cell>
-                                <Link href="https://ufl.instructure.com/ex1.cpp/download">ex1.cpp</Link></Table.Cell>
-                            <Table.Cell><Text weight="bold" color="success">Downloaded</Text></Table.Cell>
-                            <Table.Cell>
-                                <Pill color="success" margin="x-small">Graded</Pill></Table.Cell>
-                            <Table.Cell><Metric renderValue="80%" /></Table.Cell>
-                        </Table.Row>
-                        <Table.Row>
+            {/*            </Table.Row>*/}
+            {/*        </Table.Head>*/}
+            {/*        <Table.Body>*/}
+            {/*            <Table.Row>*/}
+            {/*                <Table.RowHeader>Samantha Barthelemy</Table.RowHeader>*/}
+            {/*                <Table.Cell>40475908</Table.Cell>*/}
+            {/*                <Table.Cell>*/}
+            {/*                    <Link href="https://ufl.instructure.com/ex1.cpp/download">ex1.cpp</Link></Table.Cell>*/}
+            {/*                <Table.Cell><Text weight="bold" color="success">Downloaded</Text></Table.Cell>*/}
+            {/*                <Table.Cell>*/}
+            {/*                    <Pill color="success" margin="x-small">Graded</Pill></Table.Cell>*/}
+            {/*                <Table.Cell><Metric renderValue="80%" /></Table.Cell>*/}
+            {/*            </Table.Row>*/}
+            {/*            <Table.Row>*/}
 
-                            <Table.RowHeader>Cameron Kozlin</Table.RowHeader>
-                            <Table.Cell>14055739</Table.Cell>
-                            <Table.Cell>
-                                <Link href="https://ufl.instructure.com/ex1.cpp/download">ex1.cpp</Link></Table.Cell>
-                            <Table.Cell><Text weight="bold" color="danger">Not Downloaded</Text></Table.Cell>
-                            <Table.Cell>
-                                <Pill color="danger" margin="x-small">Not Graded</Pill></Table.Cell>
-                            <Table.Cell> </Table.Cell>
-                        </Table.Row>
-                        <Table.Row>
+            {/*                <Table.RowHeader>Cameron Kozlin</Table.RowHeader>*/}
+            {/*                <Table.Cell>14055739</Table.Cell>*/}
+            {/*                <Table.Cell>*/}
+            {/*                    <Link href="https://ufl.instructure.com/ex1.cpp/download">ex1.cpp</Link></Table.Cell>*/}
+            {/*                <Table.Cell><Text weight="bold" color="danger">Not Downloaded</Text></Table.Cell>*/}
+            {/*                <Table.Cell>*/}
+            {/*                    <Pill color="danger" margin="x-small">Not Graded</Pill></Table.Cell>*/}
+            {/*                <Table.Cell> </Table.Cell>*/}
+            {/*            </Table.Row>*/}
+            {/*            <Table.Row>*/}
 
-                            <Table.RowHeader>Antonio Romero</Table.RowHeader>
-                            <Table.Cell>16952863</Table.Cell>
-                            <Table.Cell>
-                                <Link href="https://ufl.instructure.com/ex1.cpp/download">ex1.cpp</Link></Table.Cell>
-                            <Table.Cell><Text weight="bold" color="success">Downloaded</Text></Table.Cell>
-                            <Table.Cell>
-                                <Pill color="danger" margin="x-small">Not Graded</Pill></Table.Cell>
-                            <Table.Cell> </Table.Cell>
-                        </Table.Row>
-                    </Table.Body>
-                </Table>
-            </div>
+            {/*                <Table.RowHeader>Antonio Romero</Table.RowHeader>*/}
+            {/*                <Table.Cell>16952863</Table.Cell>*/}
+            {/*                <Table.Cell>*/}
+            {/*                    <Link href="https://ufl.instructure.com/ex1.cpp/download">ex1.cpp</Link></Table.Cell>*/}
+            {/*                <Table.Cell><Text weight="bold" color="success">Downloaded</Text></Table.Cell>*/}
+            {/*                <Table.Cell>*/}
+            {/*                    <Pill color="danger" margin="x-small">Not Graded</Pill></Table.Cell>*/}
+            {/*                <Table.Cell> </Table.Cell>*/}
+            {/*            </Table.Row>*/}
+            {/*        </Table.Body>*/}
+            {/*    </Table>*/}
+            {/*</div>*/}
 
         </>
     );
